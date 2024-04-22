733c071b4fd89f4efabfc5c53304613810a097b7
Patrick Walsh <pw4.2020@gmail.com>
2024-04-22 10:10:59 -0400

    add functionality to build cadence and stride plots and save to storage

---
+++ build_plots.py
@@ -0,0 +1,194 @@
+"""Program to build cadence, gait plots and save them as .png files."""
+
+import sys
+from typing import Final
+
+import numpy as np
+
+from graphical import LEG_LENGTH, ROTATE, SAMPLING, X_CENTER, read
+from scipy.interpolate import make_interp_spline
+
+
+def generate_cadence_plot(in_filename: str, out_filename: str) -> None:
+    """
+    Generate cadence plot
+    Parameters
+    ----------
+    userId : str
+        User ID
+    runId : str
+        Run ID
+    cadence : list[int]
+        Cadence
+    time : list[str]
+        Time
+
+    Returns
+    -------
+    str
+        Cadence plot link
+    """
+    # import plotly
+    import plotly.graph_objects as go
+
+    # first create the figure
+    with open(in_filename, "r") as f:
+        data: Final[list[str]] = f.read().split("\n")
+        ls, lt, rs, rt = read(data)
+
+        cadence = []
+        spm = []
+        for it in range(len(lt) - 2):
+            transfer_x = LEG_LENGTH * np.cos(lt[it][2] + ROTATE)
+            transfer_x2 = LEG_LENGTH * np.cos(lt[it + 1][2] + ROTATE)
+            if (X_CENTER + transfer_x) > X_CENTER and (
+                X_CENTER + transfer_x2
+            ) < X_CENTER:
+                cadence.append(it / SAMPLING)
+                if len(cadence) > 1:
+                    multiplier = 1 / ((cadence[-1] - cadence[0]) / 60)
+                    spm.append(round(len(cadence) * multiplier, 2))
+        # plot spm using plotly
+        go.Figure(
+            data=[
+                go.Scatter(
+                    x=[it / SAMPLING for it in range(len(spm))],
+                    y=spm,
+                    mode="lines+markers",
+                    line_shape="spline",
+                )
+            ]
+        ).write_html(out_filename)
+
+
+def generate_average_stride_plots(in_filename: str, out_filename: str) -> None:
+    """
+    Generate average stride plots
+    Parameters
+    ----------
+    userId : str
+        User ID
+    runId : str
+        Run ID
+    filename : str
+        File name
+
+    Returns
+    -------
+    str
+        Average stride plot link
+    """
+    # import plotly
+    import plotly.graph_objects as go
+
+    # first create the figure
+    with open(in_filename, "r") as f:
+        data: Final[list[str]] = f.read().split("\n")
+        ls, lt, rs, rt = read(data)
+
+        stride_length_lists = []
+        cadence = []
+
+        previous_spm_index = 0
+        spm = []
+        spm_index = []
+        spm_length = []
+        for it in range(len(lt) - 2):
+            transfer_x = LEG_LENGTH * np.cos(lt[it][2] + ROTATE)
+            transfer_x2 = LEG_LENGTH * np.cos(lt[it + 1][2] + ROTATE)
+            if (X_CENTER + transfer_x) > X_CENTER and (
+                X_CENTER + transfer_x2
+            ) < X_CENTER:
+                cadence.append(it / SAMPLING)
+                if len(cadence) > 1:
+                    multiplier = 1 / ((cadence[-1] - cadence[0]) / 60)
+                    spm_value = round(len(cadence) * multiplier, 2)
+                    if spm_value > 30:  # if cadence is greater than 30 strides a minute
+                        # add all the strides from previous_spm_index to now to list
+                        spm.append(spm_value)
+                        spm_index.append(it)
+                        spm_length.append(it - previous_spm_index)
+
+                    previous_spm_index = it
+                    if len(cadence) > 60:
+                        cadence = []
+
+        # get median spm
+        median_spm = np.percentile(spm, 0.5, interpolation="nearest")
+        # get index for median_spm
+        median_spm_index = spm.index(median_spm)
+        print(
+            f"Median spm: {median_spm}, index: {spm_index[median_spm_index]}, length: {spm_length[median_spm_index]}"
+        )
+        # plot spm using plotly
+        x = [i for i in range(spm_length[median_spm_index] + 1)]
+        xnew = np.linspace(0, spm_length[median_spm_index], 100)
+        leg_names = ["Left", "Right"]
+        y_interpolated = {}
+        for i, leg in enumerate(leg_names):
+            if leg == "Left":
+                y_just_second_index = np.subtract(
+                    [
+                        i[1]
+                        for i in lt[
+                            spm_index[median_spm_index]
+                            - spm_length[median_spm_index]
+                            - 1 : spm_index[median_spm_index]
+                        ]
+                    ],
+                    [
+                        i[1]
+                        for i in ls[
+                            spm_index[median_spm_index]
+                            - spm_length[median_spm_index]
+                            - 1 : spm_index[median_spm_index]
+                        ]
+                    ],
+                )
+            else:
+                y_just_second_index = np.subtract(
+                    [
+                        i[1]
+                        for i in rt[
+                            spm_index[median_spm_index]
+                            - spm_length[median_spm_index]
+                            - 1 : spm_index[median_spm_index]
+                        ]
+                    ],
+                    [
+                        i[1]
+                        for i in rs[
+                            spm_index[median_spm_index]
+                            - spm_length[median_spm_index]
+                            - 1 : spm_index[median_spm_index]
+                        ]
+                    ],
+                )
+            gfg = make_interp_spline(
+                x,
+                y_just_second_index,
+                k=3,
+            )
+
+            y_interpolated[leg_names[i]] = gfg(xnew)
+
+        go.Figure(
+            # make title
+            layout_title_text="Median Stride",
+            data=[
+                go.Scatter(
+                    x=xnew,
+                    y=np.rad2deg(y_interpolated[leg_name]),
+                    # name of the leg
+                    name=leg_name,
+                    mode="lines+markers",
+                )
+                for leg_name in leg_names
+            ],
+        ).write_html(out_filename)
+
+
+if __name__ == "__main__":
+    data_file_name = sys.argv[1]
+    print(data_file_name)
+    generate_average_stride_plots("user", "run", data_file_name)
--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import google
 from firebase_functions import storage_fn, options
 from firebase_admin import initialize_app, storage, firestore
 
-
+from build_plots import generate_average_stride_plots, generate_cadence_plot
 import filter_run_data
 
 # initialize firebase app
@@ -73,6 +73,26 @@ def send_image_to_storage(userId: str, image_name: str, image_link: str) -> str:
     )
 
 
+def send_html_to_storage(userId: str, html_name: str, html_link: str) -> str:
+    """
+    Send html to storage
+    Parameters
+    ----------
+    html_link : str
+        html link
+
+    Returns
+    -------
+    str
+        html link
+    """
+    storage.bucket().blob(
+        f"/plots/users/{userId}/{html_name}.html"
+    ).upload_from_filename(html_link)
+
+    return storage.bucket().blob(f"/plots/users/{userId}/{html_name}").public_url
+
+
 @storage_fn.on_object_finalized(memory=options.MemoryOption.GB_1)
 def create_video(event: storage_fn.CloudEvent[storage_fn.StorageObjectData]):
     """
@@ -112,6 +132,7 @@ def create_video(event: storage_fn.CloudEvent[storage_fn.StorageObjectData]):
     date: str = now.strftime("%Y-%m-%dT%H:%M:%SZ")
     video_link: str = f"/tmp/movies/{date}.mp4"
     os.system(f"python graphical.py {video_link}")
+
     public_link: Final[str] = send_video_to_storage(userId, f"{date}", video_link)
     # create thumbnail using first image added
     thumbnail_link: str = f"/tmp/snaps/000001.png"
@@ -140,10 +161,29 @@ def create_video(event: storage_fn.CloudEvent[storage_fn.StorageObjectData]):
         return
     # get post with type hint
     post_id: str = queryAns[0].id
+    # if /tmp/plots not directory, make it one
+    if not os.path.exists("/tmp/plots"):
+        os.makedirs("/tmp/plots")
+    # generate plots for post as well
+    stride_filename = "/tmp/plots/stride.html"
+    generate_average_stride_plots(filename, stride_filename)
+    cadence_filename = "/tmp/plots/cadence.html"
+    generate_cadence_plot(filename, cadence_filename)
+
+    # send files to storage
+    stride_public_link: Final[str] = send_html_to_storage(
+        userId, f"{date}_stride", stride_filename
+    )
+    cadence_public_link: Final[str] = send_html_to_storage(
+        userId, f"{date}_cadence", cadence_filename
+    )
+
     post: firestore.firestore.DocumentSnapshot = queryAns[0].to_dict()
     # update post
     post["videoLink"] = public_link
     post["thumbnailLink"] = thumbnail_public_link
+    post["stridePlot"] = stride_public_link
+    post["cadencePlot"] = cadence_public_link
     # update post in firebase database
     firestore_client.document(f"users/{userId}/posts/{post_id}").update(post)
     # increment numPosts on /users/{userId} by 1
